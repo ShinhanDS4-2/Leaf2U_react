796235ce462ab256b963f25acefc742507a7b826
moonkyoungmi <cher1605@naver.com>
2025-03-13 10:06:22 +0900

    스타일 수정

--- src/page/join/notice/Notice.js
+++ src/page/join/notice/Notice.js
@@ -5,11 +5,12 @@ import Button from '../../../components/button/Button';
 import BottomModal from '../../../components/modal/BottomModal';
 import Header from '../../../components/header/Header';
 import AlertModal from '../../../components/modal/AlertModal';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 const Notice = () => {
     const navigate = useNavigate();
     const modalRef = useRef();
-    const alertRef=useRef();
+    const alertRef = useRef();
 
     // 체크박스 상태 관리
     const [checkedItems, setCheckedItems] = useState({
@@ -34,7 +35,7 @@ const Notice = () => {
                 modalRef.current.openModal();
             }
         } else {
-            if(alertRef.current){
+            if (alertRef.current) {
                 alertRef.current.openModal();
             }
         }
@@ -45,57 +46,90 @@ const Notice = () => {
             <Header title={'한달적금 개설'} />
 
             <div className="notice-container">
-                <p className="notice-description">
-                    한달적금 개설을 위해 <br />
-                    약관 및 상품설명서를 꼭 확인해 주세요.
-                </p>
+                <div className="mb-3">
+                    <p className="small text-center mt-3 notice-description">
+                        한달적금 개설을 위해 <br />
+                        약관 및 상품설명서를 꼭 확인해 주세요.
+                    </p>
+                </div>
 
                 <div className="notice-item">
                     <div className="notice-header" onClick={() => toggleCheck('terms')}>
-                        <div className={`check-box ${checkedItems.terms ? 'checked' : ''}`}></div>
-                        <div className="notice-text">
-                            <h3>상품 이용 약관</h3>
-                            <br />
-                            <ul>
+                        <div className="d-flex justify-content-between">
+                            <div className="d-flex">
+                                <div
+                                    className={`check-box ${checkedItems.terms ? 'checked' : ''}`}
+                                ></div>
+
+                                <h3 className="ms-2">상품 이용 약관</h3>
+                            </div>
+                            <span className="arrow-icon" onClick={() => navigate('/terms-details')}>
+                                <Icon
+                                    icon="line-md:arrow-right"
+                                    width="20px"
+                                    height="20px"
+                                    style={{ color: 'gray' }}
+                                />
+                            </span>
+                        </div>
+                        <div className="mt-3">
+                            <ul className="notice-ul">
                                 <li>예금거래기본약관</li>
                                 <li>적립식예금약관</li>
                                 <li>비과세종합저축특약</li>
                             </ul>
                         </div>
-                        <span className="arrow-icon" onClick={() => navigate('/terms-details')}>
-                            ➔
-                        </span>
                     </div>
                 </div>
 
                 <div className="notice-item">
                     <div className="notice-header" onClick={() => toggleCheck('preGuide')}>
-                        <div
-                            className={`check-box ${checkedItems.preGuide ? 'checked' : ''}`}
-                        ></div>
-                        <div>
-                            <h3>금융상품 가입 전 안내</h3>
-                            <br />
-                            <ul>
+                        <div className="d-flex justify-content-between">
+                            <div className="d-flex">
+                                <div
+                                    className={`check-box ${
+                                        checkedItems.preGuide ? 'checked' : ''
+                                    }`}
+                                ></div>
+
+                                <h3 className="ms-2">금융상품 가입 전 안내</h3>
+                            </div>
+                            <span
+                                className="arrow-icon"
+                                onClick={() => navigate('/pre-guide-details')}
+                            >
+                                <Icon
+                                    icon="line-md:arrow-right"
+                                    width="20px"
+                                    height="20px"
+                                    style={{ color: 'gray' }}
+                                />
+                            </span>
+                        </div>
+                        <div className="mt-3">
+                            <ul className="notice-ul">
                                 <li>중요사항 설명 안내</li>
                                 <li>불이익사항 안내</li>
                                 <li>금융소비자 권리사항 안내</li>
                             </ul>
                         </div>
-                        <span className="arrow-icon" onClick={() => navigate('/pre-guide-details')}>
-                            ➔
-                        </span>
                     </div>
                 </div>
 
                 <div className="notice-item">
                     <div className="notice-header" onClick={() => toggleCheck('fraudWarning')}>
-                        <div
-                            className={`check-box ${checkedItems.fraudWarning ? 'checked' : ''}`}
-                        ></div>
-                        <div className="notice-content">
-                            <h3>불법·탈법 차명거래 금지 설명 확인</h3>
-                            <br />
+                        <div className="d-flex justify-content-between">
+                            <div className="d-flex">
+                                <div
+                                    className={`check-box ${
+                                        checkedItems.fraudWarning ? 'checked' : ''
+                                    }`}
+                                ></div>
+
+                                <h3 className="ms-2">불법·탈법 차명거래 금지 설명 확인</h3>
+                            </div>
+                        </div>
+                        <div className="mt-3">
                             <p className="notice-text">
                                 「금융실명거래 및 비밀보장에 관한 법률」 제3조 제3항에 따라 누구든지
                                 불법재산의 은닉, 자금세탁 행위, 공중협박자금 조달행위 및 강제집행의
@@ -109,14 +143,18 @@ const Notice = () => {
 
                 <div className="notice-item">
                     <div className="notice-header" onClick={() => toggleCheck('depositProtection')}>
-                        <div
-                            className={`check-box ${
-                                checkedItems.depositProtection ? 'checked' : ''
-                            }`}
-                        ></div>
-                        <div className="notice-content">
-                            <h3>예금자보호법 설명 확인</h3>
-                            <br />
+                        <div className="d-flex justify-content-between">
+                            <div className="d-flex">
+                                <div
+                                    className={`check-box ${
+                                        checkedItems.depositProtection ? 'checked' : ''
+                                    }`}
+                                ></div>
+
+                                <h3 className="ms-2">예금자보호법 설명 확인</h3>
+                            </div>
+                        </div>
+                        <div className="mt-3">
                             <p className="notice-text">
                                 본인이 가입하는 금융상품은 예금자보호법에 따라 보호됨과 보호한도에
                                 대하여 설명을 듣고 이해하였음을 확인합니다.
@@ -125,8 +163,8 @@ const Notice = () => {
                     </div>
                 </div>
             </div>
-            <div className='p-3'>
-            <Button text={'다음'} onClick={handleNextClick} />
+            <div className="p-3">
+                <Button text={'다음'} onClick={handleNextClick} />
             </div>
             <BottomModal ref={modalRef} maxHeight="50%">
                 <div className="agree-item-modal">
@@ -138,7 +176,10 @@ const Notice = () => {
                 </div>
             </BottomModal>
 
-            <AlertModal ref={alertRef} text={'모든 약관에 동의해야 다음 단계로 진행할 수 있습니다.'}/>
+            <AlertModal
+                ref={alertRef}
+                text={'모든 약관에 동의해야 다음 단계로 진행할 수 있습니다.'}
+            />
         </div>
     );
 };
--- src/page/join/start/Start.js
+++ src/page/join/start/Start.js
@@ -7,28 +7,30 @@ const Start = () => {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <div className="TextBox">
-                <h2 className="leaf">Leaf2U</h2>
-                <h2 className="month">한달적금</h2>
-            </div>
-            <div className="container">
-                <div className="TextBox2">
-                    <h2 className="text">최고금리</h2>
-                    <h2 className="text2">연 9.00%</h2>
-                    <h2 className="text3">기본 연 1.0%</h2>
+        <div className="h-100">
+            <div className="top-container">
+                <div className="TextBox">
+                    <h2 className="leaf">Leaf2U</h2>
+                    <h2 className="month">한달적금</h2>
                 </div>
-                <div className="TextBox3">
-                    <h2 className="text">적금방식</h2>
-                    <h2 className="text2">30일간 매일</h2>
-                    <h2 className="text3">우대금리 +0.1%</h2>
+                <div className="start-container">
+                    <div className="TextBox2">
+                        <h2 className="text">최고금리</h2>
+                        <h2 className="text2">연 9.00%</h2>
+                        <h2 className="text3">기본 연 1.0%</h2>
+                    </div>
+                    <div className="TextBox3">
+                        <h2 className="text">적금방식</h2>
+                        <h2 className="text2">30일간 매일</h2>
+                        <h2 className="text3">우대금리 +0.1%</h2>
+                    </div>
                 </div>
             </div>
             <div className="container2">
                 <div className="image-section">
-                    <img src={mainImg} alt="Login Icon" sizes="2" />
+                    <img src={mainImg} alt="Login Icon" />
                 </div>
-                <div className="button-container">
+                <div className="p-3">
                     <Button
                         text={'한달적금 시작하기'}
                         bgColor={'white'}
