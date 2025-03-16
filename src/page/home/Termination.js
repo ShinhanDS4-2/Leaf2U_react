0bb8ec6a854f0983a4b573992046c1259434702c
moonkyoungmi <cher1605@naver.com>
2025-03-16 15:50:26 +0900

    만기 해지 완료 화면 작업중

--- src/page/home/Termination.js
+++ src/page/home/Termination.js
@@ -1,5 +1,45 @@
+import FireworkConfetti from "../../components/effect/FireworkConfetti";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import BottomModal from "../../components/modal/BottomModal";
+import DoubleButton from "../../components/button/DoubleButton";
+
 const Termination = () => {
-    return <div></div>;
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // FireworkConfetti();
+        handleOpenBottomModal();
+    });
+
+    const bottomModalRef = useRef();
+    
+    const handleOpenBottomModal = () => {
+        if (bottomModalRef.current) {
+            bottomModalRef.current.openModal();
+        }
+    };
+
+    const handleCloseBottomModal = () => {
+        if (bottomModalRef.current) {
+            bottomModalRef.current.closeModal();
+        }
+    };
+
+    return (
+        <div>
+            {/* 하단 모달 */}
+            <BottomModal ref={bottomModalRef}>
+                <div>
+                    <div className="mt-3 mb-3">
+                        <span className="bottom-text">새로운 한달적금을<br/>다시 시작할까요?</span>
+                    </div>
+                    <DoubleButton confirmOnClick={() => {navigate("/start")}} cancelOnClick={handleCloseBottomModal}/>
+                </div>
+            </BottomModal>
+        </div>
+    );
 };
 
 export default Termination;
